190e4326ae686fe68a1756becaca2c8f06153639
Scrom <formain337@gmail.com>
2025-07-25 23:17:55 +0530

    recursion_2 second problem

--- Recursion/Recursion_2.java
+++ Recursion/Recursion_2.java
@@ -9,8 +9,20 @@ class Recursion_2 {
         }
     }
     
+    //SUMMATION OF FIRST N NUMBERS(Functional)
+    public static int sumParameterized(int i){
+        if (i == 0){
+            return 0;
+        }else{
+            return i + sumParameterized(i-1);
+        }
+    }
+    
     public static void main(String[] args) {
         int sumResult1 = sumParameterized(3,0);
         System.out.println(sumResult1);
+        
+        int sumResult2 = sumParameterized(3);
+        System.out.println(sumResult2);
     }
 }
